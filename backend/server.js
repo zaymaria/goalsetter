7f6fec472e90a3cdbb6cd0f7265f651fd0c54c9b
zaymaria <zaymaria87@gmail.com>
2024-02-02 17:29:21 +0100

    added heroku configs

--- backend/server.js
+++ backend/server.js
@@ -70,6 +70,11 @@ app.use((err, req, res, next) => {
     res.status(500).send("Internal Server Error");
 });
 
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+})
+
 app.listen (PORT, () => {
     console.log("Server is running on Port: " + PORT);
 });
